17fa88d6dc124873a665de5c1fe626687dd13caf
DESKTOP-41VGKHB\user <rkawk2239@naver.com>
2023-12-14 02:13:08 +0900

    2023-12-13

--- repository/CategoryListRepository.js
+++ repository/CategoryListRepository.js
@@ -1,56 +1,79 @@
-import { db } from '../db/database.js';
+import { db } from "../db/database.js";
 
-export async function getMall(){
-  const sql = 'select distinct mall from category';
-  return db
-  .execute(sql)
-  .then(result => result[0])
+export async function getMall() {
+  const sql = "select distinct mall from category";
+  return db.execute(sql).then((result) => result[0]);
 }
 
-export async function getFirstCategory({mall}){
-  const sql = 'select distinct firstD as categoryName, mall, firstD from category where mall= ? order by firstD';
-  return db
-  .execute(sql, [mall])
-  .then(result => result[0])
+export async function getFirstCategory({ mall }) {
+  const sql =
+    "select distinct firstD as categoryName, mall, firstD from category where mall= ? order by firstD";
+  return db.execute(sql, [mall]).then((result) => result[0]);
 }
 
-export async function getSecondDCategory( { mall, firstD }){
-  const sql = 'select distinct secondD as categoryName, mall, firstD, secondD from category where mall = ? and firstD = ? and secondD is not null order by secondD';
-  return db
-  .execute(sql, [mall, firstD])
-  .then(result => result[0])
+export async function getSecondDCategory({ mall, firstD }) {
+  const sql =
+    "select distinct secondD as categoryName, mall, firstD, secondD from category where mall = ? and firstD = ? and secondD is not null order by secondD";
+  return db.execute(sql, [mall, firstD]).then((result) => result[0]);
 }
 
-export async function getThirdDCategory( { mall, firstD, secondD }){
-  const sql = 'select distinct thirdD as categoryName, mall, firstD, secondD, thirdD from category where mall = ? and firstD = ? and secondD = ? and thirdD is not null order by thirdD';
-  return db
-  .execute(sql, [mall, firstD, secondD])
-  .then(result => result[0])
+export async function getThirdDCategory({ mall, firstD, secondD }) {
+  const sql =
+    "select distinct thirdD as categoryName, mall, firstD, secondD, thirdD from category where mall = ? and firstD = ? and secondD = ? and thirdD is not null order by thirdD";
+  return db.execute(sql, [mall, firstD, secondD]).then((result) => result[0]);
 }
 
-export async function getFourthDCategory( { mall, firstD, secondD, thirdD }){
-  const sql = 'select distinct fourthD as categoryName, mall, firstD, secondD, thirdD, fourthD from category where mall = ? and firstD = ? and secondD = ? and thirdD = ? and fourthD is not null order by fourthD';
+export async function getFourthDCategory({ mall, firstD, secondD, thirdD }) {
+  const sql =
+    "select distinct fourthD as categoryName, mall, firstD, secondD, thirdD, fourthD from category where mall = ? and firstD = ? and secondD = ? and thirdD = ? and fourthD is not null order by fourthD";
   return db
-  .execute(sql, [mall, firstD, secondD, thirdD])
-  .then(result => result[0])
+    .execute(sql, [mall, firstD, secondD, thirdD])
+    .then((result) => result[0]);
 }
 
-export async function getFifthDCategory( { mall, firstD, secondD, thirdD, fourthD }){
+export async function getFifthDCategory({
+  mall,
+  firstD,
+  secondD,
+  thirdD,
+  fourthD,
+}) {
   let parameter = [mall, firstD, secondD, thirdD, fourthD];
-  let sql = 'select distinct fifthD as categoryName, mall, firstD, secondD, thirdD, fourthD, fifthD from category where mall = ? and firstD = ? and secondD = ? and thirdD = ? and fourthD = ? and fifthD is not null order by fifthD';
+  let sql =
+    "select distinct fifthD as categoryName, mall, firstD, secondD, thirdD, fourthD, fifthD from category where mall = ? and firstD = ? and secondD = ? and thirdD = ? and fourthD = ? and fifthD is not null order by fifthD";
 
-  if(secondD === 'undefined'){
-    sql = 'select distinct secondD as categoryName, mall, firstD, secondD from category where mall = ? and firstD = ? and secondD is not null order by secondD';
+  if (secondD === "undefined") {
+    sql =
+      "select distinct secondD as categoryName, mall, firstD, secondD from category where mall = ? and firstD = ? and secondD is not null order by secondD";
     parameter = [mall, firstD];
-  } else if (thirdD === 'undefined') {
-    sql = 'select distinct thirdD as categoryName, mall, firstD, secondD, thirdD from category where mall = ? and firstD = ? and secondD = ? and thirdD is not null order by thirdD';
+  } else if (thirdD === "undefined") {
+    sql =
+      "select distinct thirdD as categoryName, mall, firstD, secondD, thirdD from category where mall = ? and firstD = ? and secondD = ? and thirdD is not null order by thirdD";
     parameter = [mall, firstD, secondD];
-  } else if (fourthD === 'undefined') {
-    sql = 'select distinct fourthD as categoryName, mall, firstD, secondD, thirdD, fourthD from category where mall = ? and firstD = ? and secondD = ? and thirdD = ? and fourthD is not null order by fourthD';
+  } else if (fourthD === "undefined") {
+    sql =
+      "select distinct fourthD as categoryName, mall, firstD, secondD, thirdD, fourthD from category where mall = ? and firstD = ? and secondD = ? and thirdD = ? and fourthD is not null order by fourthD";
     parameter = [mall, firstD, secondD, thirdD];
   }
 
+  return db.execute(sql, parameter).then((result) => result[0]);
+}
+
+export async function getBookData({mall, firstD, secondD, thirdD, fourthD, startIndex, endIndex, sorting}) {
+  let categoryName = `${mall}>${firstD}${secondD !== "undefined" ? ">" + secondD : ""}${thirdD !== "undefined" ? ">" + thirdD : ""}${fourthD !== "undefined" ? ">" + fourthD : ""}%`;
+  let sql = `select
+	categoryName, priceSales, totalResults, rno, title, author, pubDate, description, isbn13, priceStandard, mallType, stockStatus, mileage, cover, publisher, salesPoint, customerReviewRank
+from
+	(select
+		categoryName, priceSales, row_number() over(order by ${sorting}) as rno, totalResults, title, author, pubDate, description, isbn13, priceStandard, mallType, stockStatus, mileage, cover, publisher, salesPoint, customerReviewRank
+	from
+		book_all,
+        (select count(*) as totalResults from book_all where categoryName like ?) book_totalResults
+	where 
+		categoryName like ?) bookList
+	where rno between ? and ?`;
+
   return db
-  .execute(sql, parameter)
-  .then(result => result[0])
+    .execute(sql, [categoryName, categoryName, startIndex, endIndex])
+    .then((result) => result[0]);
 }
--- router/CategoryListRouter.js
+++ router/CategoryListRouter.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import * as CategoryListController from '../controller/CategoryListController.js';
+import express from "express";
+import * as CategoryListController from "../controller/CategoryListController.js";
 
 const router = express.Router();
 
-router.get('/mall', CategoryListController.getMall);
-router.get('/:mall', CategoryListController.getFirstCategory);
-router.get('/:mall/:firstD', CategoryListController.getSecondDCategory);
-router.get('/:mall/:firstD/:secondD', CategoryListController.getThirdDCategory);
-router.get('/:mall/:firstD/:secondD/:thirdD', CategoryListController.getFourthDCategory);
-router.get('/:mall/:firstD/:secondD/:thirdD/:fourthD', CategoryListController.getFifthDCategory);
+router.get("/mall", CategoryListController.getMall);
+router.get("/:mall", CategoryListController.getFirstCategory);
+router.get("/:mall/:firstD", CategoryListController.getSecondDCategory);
+router.get("/:mall/:firstD/:secondD", CategoryListController.getThirdDCategory);
+router.get("/:mall/:firstD/:secondD/:thirdD", CategoryListController.getFourthDCategory);
+router.get("/:mall/:firstD/:secondD/:thirdD/:fourthD", CategoryListController.getFifthDCategory);
+router.get("/:mall/:firstD/:secondD/:thirdD/:fourthD/:startIndex/:endIndex/:sorting", CategoryListController.getBookData);
 
 export default router;
